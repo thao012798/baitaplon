460adb8a3b5a88cc6e7e370f3c393275a2c8b7ac
NhungTH3 <44947102+NhungTH3@users.noreply.github.com>
2018-12-04 22:33:36 +0700

    đây nhé

--- code.js
+++
@@ -1,9 +0,0 @@
-$(window).resize(resizehappend)
-
-
-function resizehappend(){
-    var x= $(window).width();
-    var y= $(window).height();
-    $('#status').html("trinh duyet rong"+x+"px va cao"+y+"px")
-}
-
